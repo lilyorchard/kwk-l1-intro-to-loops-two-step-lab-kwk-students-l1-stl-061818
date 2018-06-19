c6a16d67e8f2f34d17380d1d6afcabbf772810be
lilyorchard <lily@orchardfamily.com>
2018-06-19 20:23:46 +0000

    automatically backed up by learn

---
+++ counter_loops.rb
@@ -0,0 +1,21 @@
+def cha_cha_slide
+  moves_completed = 0 
+  
+  loop do
+    
+    if moves_completed == 10
+      break
+    end
+    
+    puts "slide to the left"
+    sleep(0.1)
+    puts "slide to the right"
+    sleep(0.1)
+    puts "and kick"
+end
+
+
+
+
+
+end
