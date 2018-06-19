c8a0fad5802debc8032a17ede989b3c469401415
lilyorchard <lily@orchardfamily.com>
2018-06-19 20:37:50 +0000

    automatically backed up by learn

--- beats_left.rb
+++ beats_left.rb
@@ -6,6 +6,18 @@ def dance
     if beats_left_in_song == 0
       break
     end
+    puts "slide to the left"
+    sleep(0.1)
+    puts "slide to the right"
+    sleep(0.1)
+    puts "and kick"
+    sleep(0.1)
+moves_completed = moves_completed - 6
+
+puts "you've completed #{moves_completed}. Don't listen to what they say. You are a great dancer."
+end
+end
+dance
     
     
   
