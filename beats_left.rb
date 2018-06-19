99a64c26e4af7f5515be37d7c69786a30ac0edd9
lilyorchard <lily@orchardfamily.com>
2018-06-19 20:36:14 +0000

    automatically backed up by learn

--- beats_left.rb
+++ beats_left.rb
@@ -1,8 +1,13 @@
-def
+def dance
   beats_left_in_song = 60
   
-  if #finish this line, you should stop when beats == 0 
-  
+  loop do
+    
+    if beats_left_in_song == 0
+      break
+    end
+    
+    
   
   
 end
